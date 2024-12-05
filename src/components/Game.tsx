a0c782ca32be8fabdaea8b2af97c02c812681bd5
eskilschjong <90253352+eskilschjong@users.noreply.github.com>
2024-12-05 06:21:26 +0100

    refactor: Remove unused logo import and clean up Game component imports

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Circle, CircleFill, ArrowCounterclockwise, GearFill } from 'react-bootstrap-icons';
+import { ArrowCounterclockwise, GearFill } from 'react-bootstrap-icons';
 import GameOverModal from './GameOverModal';
 import SettingsModal from './SettingsModal';
 
